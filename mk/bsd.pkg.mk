wiedi/pkgsrc

Implement multi-architecture support.
This commit adds a number of new features to pkgsrc to enable
multi-architecture packages:

  - pkgtools/isaexec is a new package which integrates the isaexec
    wrapper from Solaris.  Multi-architecture binaries are hardlinks
    to this file, which then determines the architecture-dependent
    binary to run.

  - pkg_install grows "@link" support to allow isaexec hardlinks to
    be created at pkg_add time.

  - mk grows a MULTIARCH variable, set by --multiarch in bootstrap,
    which determines whether or not to build multiarch packages.

  - Each phase target will run twice (once for each architecture)
    when MULTIARCH is enabled, and the final package will combine the
    output of each build.

As for package support:

  - Packages opt-in to multi-architecture support by setting
    USE_MULTIARCH=yes, but this is only activated based on MULTIARCH.

  - BINARCHSUFFIX and LIBARCHSUFFIX will expand to directory suffixes
    for the current ABI, and will normally be used where there are
    hardcoded 'bin' or 'lib' paths.

  - When MULTIARCH is enabled, pkgsrc will build each package twice
    (once for each architecture), and then combine the two into a
    single package.

This is currently only supported on SunOS, but it is generic enough
that support can easily be added for other platforms.

mk/bsd.pkg.mk
@@ -99,6 +99,19 @@ SVR4_PKGNAME?=		${PKGNAME}
 TOOL_DEPENDS?=		# empty
 WRKSRC?=		${WRKDIR}/${DISTNAME}
 
+# Multi-architecture support
+.if !empty(MULTIARCH:M[Yy][Ee][Ss]) && defined(USE_MULTIARCH)
+_MULTIARCH=     	YES
+BINARCHSUFFIX=		${BINARCHSUFFIX.${ABI}}
+INCARCHSUFFIX=		${INCARCHSUFFIX.${ABI}}
+LIBARCHSUFFIX=		${LIBARCHSUFFIX.${ABI}}
+.  if ${OPSYS} == "SunOS" && !empty(USE_MULTIARCH:Mbin)
+DEPENDS+=		isaexec-[0-9]*:../../pkgtools/isaexec
+.  endif
+MULTIARCH_DIRS.bin?=	bin sbin
+MULTIARCH_DIRS.lib?=	lib
+.endif
+
 # Override for SU_CMD user check
 _IS_ROOT_CMD?=		${TEST}	`${ID} -u` = `${ID} -u ${_SU_ROOT_USER}`
 _SU_ROOT_USER?=		${ROOT_USER}
@@ -159,6 +172,7 @@ CPPFLAGS+=	${CPP_PRECOMP_FLAGS}
 # to dependence builds.
 PKGSRC_SETENV?=	${SETENV}
 
+ALL_ENV+=	BINARCHSUFFIX=${BINARCHSUFFIX:Q}
 ALL_ENV+=	CC=${CC:Q}
 ALL_ENV+=	CFLAGS=${CFLAGS:M*:Q}
 ALL_ENV+=	CPPFLAGS=${CPPFLAGS:M*:Q}
@@ -177,6 +191,7 @@ ALL_ENV+=	LC_MONETARY=C
 ALL_ENV+=	LC_NUMERIC=C
 ALL_ENV+=	LC_TIME=C
 ALL_ENV+=	LDFLAGS=${LDFLAGS:M*:Q}
+ALL_ENV+=	LIBARCHSUFFIX=${LIBARCHSUFFIX:Q}
 ALL_ENV+=	LINKER_RPATH_FLAG=${LINKER_RPATH_FLAG:Q}
 ALL_ENV+=	PATH=${PATH:Q}:${LOCALBASE}/bin:${X11BASE}/bin
 ALL_ENV+=	PREFIX=${PREFIX}
@@ -189,9 +204,9 @@ BSD_MAKE_ENV+=	MANOWN=${MANOWN} MANGRP=${MANGRP}
 BSD_MAKE_ENV+=	SHAREOWN=${SHAREOWN} SHAREGRP=${SHAREGRP}
 BSD_MAKE_ENV+=	DOCOWN=${DOCOWN} DOCGRP=${DOCGRP}
 BSD_MAKE_ENV+=	BINMODE=${BINMODE} NONBINMODE=${NONBINMODE}
-BSD_MAKE_ENV+=	BINDIR=${PREFIX}/bin
+BSD_MAKE_ENV+=	BINDIR=${PREFIX}/bin${BINARCHSUFFIX}
 BSD_MAKE_ENV+=	INCSDIR=${PREFIX}/include
-BSD_MAKE_ENV+=	LIBDIR=${PREFIX}/lib
+BSD_MAKE_ENV+=	LIBDIR=${PREFIX}/lib${LIBARCHSUFFIX}
 BSD_MAKE_ENV+=	MANDIR=${PREFIX}/${PKGMANDIR}
 BSD_MAKE_ENV+=	STRIPFLAG=${_STRIPFLAG_INSTALL:Q}
 BSD_MAKE_ENV+=	MANINSTALL=${MANINSTALL:Q}
@@ -240,6 +255,11 @@ _NULL_COOKIE=		${WRKDIR}/.null
 SHCOMMENT?=		${ECHO_MSG} >/dev/null '***'
 
 LIBABISUFFIX?=
+
+# Multi-architecture builds
+BINARCHSUFFIX?=
+INCARCHSUFFIX?=
+LIBARCHSUFFIX?=
 
 TOUCH_FLAGS?=		-f
 

mk/plist/plist-multiarch.awk
@@ -1,0 +1,123 @@
+# $NetBSD$
+#
+# Copyright (c) 2013 Joyent, Inc.  All rights reserved.
+#
+# This code is derived from software contributed to The NetBSD Foundation
+# by Jonathan Perkin <jperkin@joyent.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+#
+# Convert ${{BIN,INC,LIB}ARCHSUFFIX} into lines for each supported ABI.
+#
+# Requires that for each var in MULTIARCH_ABIS there is a corresponding
+# {BIN,INC,LIB}ARCHSUFFIX_${var} environment variable set pointing to the
+# appropriate directory.
+#
+BEGIN {
+	PLIST_MULTIARCH_ENABLED = ENVIRON["_MULTIARCH"] ? 1 : 0
+	PLIST_USE_MULTIARCH = ENVIRON["USE_MULTIARCH"] ? ENVIRON["USE_MULTIARCH"] : ""
+	PLIST_MULTIARCH_ABIS = ENVIRON["MULTIARCH_ABIS"]
+	split(PLIST_MULTIARCH_ABIS, abis, " ")
+	PLIST_MULTIARCH_DIRS_bin = ENVIRON["MULTIARCH_DIRS_bin"] ? ENVIRON["MULTIARCH_DIRS_bin"] : ""
+	PLIST_MULTIARCH_DIRS_lib = ENVIRON["MULTIARCH_DIRS_lib"] ? ENVIRON["MULTIARCH_DIRS_lib"] : ""
+	split(PLIST_MULTIARCH_DIRS_bin, bindirs, " ")
+	split(PLIST_MULTIARCH_DIRS_lib, libdirs, " ")
+	PLIST_MULTIARCH_SKIP_DIRS_bin = ENVIRON["MULTIARCH_SKIP_DIRS_bin"] ? ENVIRON["MULTIARCH_SKIP_DIRS_bin"] : ""
+	PLIST_MULTIARCH_SKIP_DIRS_lib = ENVIRON["MULTIARCH_SKIP_DIRS_lib"] ? ENVIRON["MULTIARCH_SKIP_DIRS_lib"] : ""
+	split(PLIST_MULTIARCH_SKIP_DIRS_bin, skipbindirs, " ")
+	split(PLIST_MULTIARCH_SKIP_DIRS_lib, skiplibdirs, " ")
+}
+
+function replace_arch_dirs(type, dirs, skipdirs, suffixvar)
+{
+	matched = 0
+	for (dir in dirs) {
+		dirmatch = "^" dirs[dir] "/"
+		if ($0 ~ dirmatch) {
+			skip = 0
+			for (skipdir in skipdirs) {
+				skipdirmatch = "^" skipdirs[skipdir] "/"
+				if ($0 ~ skipdirmatch) {
+					skip = 1
+				}
+			}
+			if (skip) {
+				continue
+			}
+			for (abi in abis) {
+				line = $0
+				matched += gsub(dirmatch, dirs[dir] ENVIRON[suffixvar "_" abis[abi]] "/", line)
+				print_entry(line)
+			}
+		}
+	}
+	# XXX: Limit to SunOS only
+	if (type == "bin" && matched) {
+		print_entry("@link lib/isaexec " $0)
+	}
+	return matched
+}
+
+PLIST_MULTIARCH_ENABLED && PLIST_USE_MULTIARCH ~ /bin/ {
+	if (replace_arch_dirs("bin", bindirs, skipbindirs, "BINARCHSUFFIX") > 0) {
+		next
+	}
+}
+
+PLIST_MULTIARCH_ENABLED && PLIST_USE_MULTIARCH ~ /lib/ {
+	if (replace_arch_dirs("lib", libdirs, skiplibdirs, "LIBARCHSUFFIX") > 0) {
+		next
+	}
+}
+
+# Support manual PLIST entries.
+PLIST_MULTIARCH_ENABLED && (/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/) {
+	isalink = 0
+	for (abi in abis) {
+		binval = ENVIRON["BINARCHSUFFIX_" abis[abi]]
+		incval = ENVIRON["INCARCHSUFFIX_" abis[abi]]
+		libval = ENVIRON["LIBARCHSUFFIX_" abis[abi]]
+		line = $0
+		isalink += gsub(/[$][{]BINARCHSUFFIX[}]/, binval, line)
+		gsub(/[$][{]INCARCHSUFFIX[}]/, incval, line)
+		gsub(/[$][{]LIBARCHSUFFIX[}]/, libval, line)
+		print_entry(line)
+	}
+	# XXX: Limit to SunOS only
+	if (isalink) {
+		bin = $0
+		gsub(/[$][{]BINARCHSUFFIX[}]/, "", bin)
+		print_entry("@link lib/isaexec " bin)
+	}
+	if (/[$][{]INCARCHSUFFIX[}]/) {
+		gsub(/[$][{]INCARCHSUFFIX[}]/, "")
+		print_entry($0)
+	}
+	next
+}
+
+/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/ {
+	gsub(/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/, "")
+}

TritonDataCenter/pkgsrc-legacy

  - pkgtools/abiexec is a new package which integrates the isaexec
    binary to run, with 32-bit being the default.
  - pkg_install grows "@link" support to allow abiexec hardlinks to
  - Support is added to lang/gcc47 for the ABI environment variable to
    control -m32/-m64.

mk/bsd.pkg.mk
@@ -102,6 +102,19 @@ WRKSRC?=		${WRKDIR}/${GITHUB_PROJECT}-${GITHUB_TAG:C/^v//}
 WRKSRC?=		${WRKDIR}/${DISTNAME:U${PKGNAME_NOREV}}
 .endif
 
+# Multi-architecture support
+.if !empty(MULTIARCH:M[Yy][Ee][Ss]) && defined(USE_MULTIARCH)
+_MULTIARCH=     	YES
+BINARCHSUFFIX=		${BINARCHSUFFIX.${ABI}}
+INCARCHSUFFIX=		${INCARCHSUFFIX.${ABI}}
+LIBARCHSUFFIX=		${LIBARCHSUFFIX.${ABI}}
+.  if ${OPSYS} == "SunOS" && !empty(USE_MULTIARCH:Mbin)
+DEPENDS+=		abiexec-[0-9]*:../../pkgtools/abiexec
+.  endif
+MULTIARCH_DIRS.bin?=	bin sbin
+MULTIARCH_DIRS.lib?=	lib
+.endif
+
 # Override for SU_CMD user check
 _IS_ROOT_CMD?=		${TEST}	`${ID} -u` = `${ID} -u ${_SU_ROOT_USER}`
 _SU_ROOT_USER?=		${ROOT_USER}
@@ -158,6 +171,7 @@ CPPFLAGS+=	${CPP_PRECOMP_FLAGS}
 # to dependence builds.
 PKGSRC_SETENV?=	${SETENV}
 
+ALL_ENV+=	BINARCHSUFFIX=${BINARCHSUFFIX:Q}
 ALL_ENV+=	CC=${CC:Q}
 ALL_ENV+=	CFLAGS=${CFLAGS:M*:Q}
 ALL_ENV+=	CPPFLAGS=${CPPFLAGS:M*:Q}
@@ -176,6 +190,7 @@ ALL_ENV+=	LC_MONETARY=C
 ALL_ENV+=	LC_NUMERIC=C
 ALL_ENV+=	LC_TIME=C
 ALL_ENV+=	LDFLAGS=${LDFLAGS:M*:Q}
+ALL_ENV+=	LIBARCHSUFFIX=${LIBARCHSUFFIX:Q}
 ALL_ENV+=	LINKER_RPATH_FLAG=${LINKER_RPATH_FLAG:Q}
 ALL_ENV+=	PATH=${PATH:Q}:${LOCALBASE}/bin:${X11BASE}/bin
 ALL_ENV+=	PREFIX=${PREFIX}
@@ -190,9 +205,9 @@ BSD_MAKE_ENV+=	MANOWN=${MANOWN} MANGRP=${MANGRP}
 BSD_MAKE_ENV+=	SHAREOWN=${SHAREOWN} SHAREGRP=${SHAREGRP}
 BSD_MAKE_ENV+=	DOCOWN=${DOCOWN} DOCGRP=${DOCGRP}
 BSD_MAKE_ENV+=	BINMODE=${BINMODE} NONBINMODE=${NONBINMODE}
-BSD_MAKE_ENV+=	BINDIR=${PREFIX}/bin
+BSD_MAKE_ENV+=	BINDIR=${PREFIX}/bin${BINARCHSUFFIX}
 BSD_MAKE_ENV+=	INCSDIR=${PREFIX}/include
-BSD_MAKE_ENV+=	LIBDIR=${PREFIX}/lib
+BSD_MAKE_ENV+=	LIBDIR=${PREFIX}/lib${LIBARCHSUFFIX}
 BSD_MAKE_ENV+=	MANDIR=${PREFIX}/${PKGMANDIR}
 BSD_MAKE_ENV+=	STRIPFLAG=${_STRIPFLAG_INSTALL:Q}
 BSD_MAKE_ENV+=	MANINSTALL=${MANINSTALL:Q}
@@ -237,6 +252,11 @@ _NULL_COOKIE=		${WRKDIR}/.null
 SHCOMMENT?=		${ECHO_MSG} >/dev/null '***'
 
 LIBABISUFFIX?=
+
+# Multi-architecture builds
+BINARCHSUFFIX?=
+INCARCHSUFFIX?=
+LIBARCHSUFFIX?=
 
 TOUCH_FLAGS?=		-f
 

mk/plist/plist-multiarch.awk
@@ -1,0 +1,123 @@
+# $NetBSD$
+#
+# Copyright (c) 2013 Joyent, Inc.  All rights reserved.
+#
+# This code is derived from software contributed to The NetBSD Foundation
+# by Jonathan Perkin <jperkin@joyent.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+#
+# Convert ${{BIN,INC,LIB}ARCHSUFFIX} into lines for each supported ABI.
+#
+# Requires that for each var in MULTIARCH_ABIS there is a corresponding
+# {BIN,INC,LIB}ARCHSUFFIX_${var} environment variable set pointing to the
+# appropriate directory.
+#
+BEGIN {
+	PLIST_MULTIARCH_ENABLED = ENVIRON["_MULTIARCH"] ? 1 : 0
+	PLIST_USE_MULTIARCH = ENVIRON["USE_MULTIARCH"] ? ENVIRON["USE_MULTIARCH"] : ""
+	PLIST_MULTIARCH_ABIS = ENVIRON["MULTIARCH_ABIS"]
+	split(PLIST_MULTIARCH_ABIS, abis, " ")
+	PLIST_MULTIARCH_DIRS_bin = ENVIRON["MULTIARCH_DIRS_bin"] ? ENVIRON["MULTIARCH_DIRS_bin"] : ""
+	PLIST_MULTIARCH_DIRS_lib = ENVIRON["MULTIARCH_DIRS_lib"] ? ENVIRON["MULTIARCH_DIRS_lib"] : ""
+	split(PLIST_MULTIARCH_DIRS_bin, bindirs, " ")
+	split(PLIST_MULTIARCH_DIRS_lib, libdirs, " ")
+	PLIST_MULTIARCH_SKIP_DIRS_bin = ENVIRON["MULTIARCH_SKIP_DIRS_bin"] ? ENVIRON["MULTIARCH_SKIP_DIRS_bin"] : ""
+	PLIST_MULTIARCH_SKIP_DIRS_lib = ENVIRON["MULTIARCH_SKIP_DIRS_lib"] ? ENVIRON["MULTIARCH_SKIP_DIRS_lib"] : ""
+	split(PLIST_MULTIARCH_SKIP_DIRS_bin, skipbindirs, " ")
+	split(PLIST_MULTIARCH_SKIP_DIRS_lib, skiplibdirs, " ")
+}
+
+function replace_arch_dirs(type, dirs, skipdirs, suffixvar)
+{
+	matched = 0
+	for (dir in dirs) {
+		dirmatch = "^" dirs[dir] "/"
+		if ($0 ~ dirmatch) {
+			skip = 0
+			for (skipdir in skipdirs) {
+				skipdirmatch = "^" skipdirs[skipdir] "/"
+				if ($0 ~ skipdirmatch) {
+					skip = 1
+				}
+			}
+			if (skip) {
+				continue
+			}
+			for (abi in abis) {
+				line = $0
+				matched += gsub(dirmatch, dirs[dir] ENVIRON[suffixvar "_" abis[abi]] "/", line)
+				print_entry(line)
+			}
+		}
+	}
+	# XXX: Limit to SunOS only
+	if (type == "bin" && matched) {
+		print_entry("@link lib/abiexec " $0)
+	}
+	return matched
+}
+
+PLIST_MULTIARCH_ENABLED && PLIST_USE_MULTIARCH ~ /bin/ {
+	if (replace_arch_dirs("bin", bindirs, skipbindirs, "BINARCHSUFFIX") > 0) {
+		next
+	}
+}
+
+PLIST_MULTIARCH_ENABLED && PLIST_USE_MULTIARCH ~ /lib/ {
+	if (replace_arch_dirs("lib", libdirs, skiplibdirs, "LIBARCHSUFFIX") > 0) {
+		next
+	}
+}
+
+# Support manual PLIST entries.
+PLIST_MULTIARCH_ENABLED && (/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/) {
+	isalink = 0
+	for (abi in abis) {
+		binval = ENVIRON["BINARCHSUFFIX_" abis[abi]]
+		incval = ENVIRON["INCARCHSUFFIX_" abis[abi]]
+		libval = ENVIRON["LIBARCHSUFFIX_" abis[abi]]
+		line = $0
+		isalink += gsub(/[$][{]BINARCHSUFFIX[}]/, binval, line)
+		gsub(/[$][{]INCARCHSUFFIX[}]/, incval, line)
+		gsub(/[$][{]LIBARCHSUFFIX[}]/, libval, line)
+		print_entry(line)
+	}
+	# XXX: Limit to SunOS only
+	if (isalink) {
+		bin = $0
+		gsub(/[$][{]BINARCHSUFFIX[}]/, "", bin)
+		print_entry("@link lib/abiexec " bin)
+	}
+	if (/[$][{]INCARCHSUFFIX[}]/) {
+		gsub(/[$][{]INCARCHSUFFIX[}]/, "")
+		print_entry($0)
+	}
+	next
+}
+
+/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/ {
+	gsub(/[$][{](BIN|INC|LIB)ARCHSUFFIX[}]/, "")
+}