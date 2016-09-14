83475d95fb5a3ef0359adf96f201526ccee6ea9f
Jonathan Perkin <jperkin@joyent.com>
2016-09-14 12:13:11 +0100

    lang/python35: multiarch fixes

--- lang/python35/patches/patch-Lib_distutils_command_install.py
+++ lang/python35/patches/patch-Lib_distutils_command_install.py
@@ -4,7 +4,7 @@ Support multiarch.
 
 --- Lib/distutils/command/install.py.orig	2012-02-23 20:22:44.000000000 +0000
 +++ Lib/distutils/command/install.py
-@@ -44,8 +44,8 @@
+@@ -29,8 +29,8 @@ WINDOWS_SCHEME = {
  
  INSTALL_SCHEMES = {
      'unix_prefix': {
@@ -15,7 +15,7 @@ Support multiarch.
          'headers': '$base/include/python$py_version_short$abiflags/$dist_name',
          'scripts': '$base/bin',
          'data'   : '$base',
-@@ -311,6 +311,10 @@
+@@ -281,6 +281,10 @@ class install(Command):
  
          py_version = sys.version.split()[0]
          (prefix, exec_prefix) = get_config_vars('prefix', 'exec_prefix')
@@ -26,7 +26,7 @@ Support multiarch.
          try:
              abiflags = sys.abiflags
          except AttributeError:
-@@ -327,6 +331,7 @@
+@@ -297,6 +301,7 @@ class install(Command):
                              'sys_exec_prefix': exec_prefix,
                              'exec_prefix': exec_prefix,
                              'abiflags': abiflags,
@@ -34,7 +34,7 @@ Support multiarch.
                             }
  
          if HAS_USER_SITE:
-@@ -676,5 +676,6 @@ class install(Command):
+@@ -646,5 +651,6 @@ class install(Command):
                      ('install_headers', has_headers),
                      ('install_scripts', has_scripts),
                      ('install_data',    has_data),
