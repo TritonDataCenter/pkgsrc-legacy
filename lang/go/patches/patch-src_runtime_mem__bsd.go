c6f661a661b317f3b2845f8383230b8aec169fad
Sean Chittenden <sean@chittenden.org>
2017-04-28 15:28:03 -0700

    Improve handling of low-memory situations on Illumos.
    On Illumos `mmap(2)` can return `EAGAIN` when a process is running
    low on available RSS.  At best, not trapping `EAGAIN` leads to misleading
    error messages.

---
+++ lang/go/patches/patch-src_runtime_mem__bsd.go
@@ -0,0 +1,20 @@
+--- src/runtime/mem_bsd.go.orig	2017-04-28 12:14:22.000000000 -0700
++++ src/runtime/mem_bsd.go	2017-04-28 12:14:31.000000000 -0700
+@@ -76,7 +76,7 @@
+ 			flags |= _MAP_FIXED
+ 		}
+ 		p := mmap(v, n, _PROT_READ|_PROT_WRITE, flags, -1, 0)
+-		if uintptr(p) == _ENOMEM {
++		if uintptr(p) == _ENOMEM || (GOOS == "solaris" && uintptr(p) == _EAGAIN) {
+ 			throw("runtime: out of memory")
+ 		}
+ 		if p != v {
+@@ -87,7 +87,7 @@
+ 	}
+ 
+ 	p := mmap(v, n, _PROT_READ|_PROT_WRITE, _MAP_ANON|_MAP_FIXED|_MAP_PRIVATE, -1, 0)
+-	if uintptr(p) == _ENOMEM {
++	if uintptr(p) == _ENOMEM || (GOOS == "solaris" && uintptr(p) == _EAGAIN) {
+ 		throw("runtime: out of memory")
+ 	}
+ 	if p != v {
