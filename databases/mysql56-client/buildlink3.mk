e7fafac11e20d7033e2f4c6e1c970dd015548da1
Filip Hajny <filip@joyent.com>
2017-08-07 18:04:28 +0000

    Backport MySQL updates

--- databases/mysql56-client/buildlink3.mk
+++ databases/mysql56-client/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/12 23:17:39 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2016/10/02 19:21:30 fhajny Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MYSQL_CLIENT_BUILDLINK3_MK)
 MYSQL_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.6.10
-BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.6.15nb1
+BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.6.10<5.7
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.6.29nb1
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql56-client
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 BUILDLINK_LIBDIRS.mysql-client?=	lib
